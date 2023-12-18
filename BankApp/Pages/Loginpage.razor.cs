02e1a2d8abfa0e099835f221cceab11e09197f08
Abagado <147521764+Abagado@users.noreply.github.com>
2023-12-18 15:47:07 +1000

    Loginpage, Address, Course

---
+++ BankApp/Pages/Loginpage.razor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+namespace MauiApp1.Components.Pages
+{
+    public partial class Loginpage : ComponentBase
+    {
+        private bool showDialog = false;
+        private string login = "";
+        private string password = "";
+        private bool loggedIn = false;
+
+        private void ShowLoginDialog()
+        {
+            showDialog = true;
+        }
+
+        private void Login()
+        {
+            // Здесь можно добавить обработку входа пользователя
+            // на основе введенного логина и пароля
+
+            showDialog = false;
+            loggedIn = true;
+        }
+
+        private void Cancel()
+        {
+            showDialog = false;
+        }
+    }
+}
